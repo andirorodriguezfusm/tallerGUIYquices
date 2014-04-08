8ef2ba35ddc28566cf292fe3be85304128deafac
Andrew Ditson Rodriguez <ar131505@ingenieria.sanmartin.edu.co>
2014-04-08 14:55:10 -0500

    cambio

--- TallerGUI/Assets/NewBehaviourScript.cs
+++ TallerGUI/Assets/NewBehaviourScript.cs
@@ -5,10 +5,7 @@ public class NewBehaviourScript : MonoBehaviour {
 		int pos =0;
 		int pos1=0;
 		int pos2=0;
-	private int button=0;
-	private int plano=0;
-	private int esfera=0;
-	private int capsula=0;
+
 
 		float r = 0.0f;
 		float g = 0.0f;
@@ -30,59 +27,65 @@ public class NewBehaviourScript : MonoBehaviour {
 				b = GUI.HorizontalSlider (new Rect (450, 70, 100, 30), b, 0.0f, 1.0f);
 			
 				if (GUI.Button (new Rect (250, 50, 100, 30), "Crear Cubo")) {
-						if (button == 0) {
+						
 								pos += 3;
 								cube1 = GameObject.CreatePrimitive (PrimitiveType.Cube);
 								cube1.transform.position = new Vector3 (-8, 0, pos);
 								cube1.transform.localScale = new Vector3 (2, 2, 2);
 								cube1.transform.rotation = Quaternion.Euler (0, 0, 0);
 								cube1.renderer.material.color = new Color (r, g, b, 250f);
-								button = 1;
+								
+				Destroy(esfera1,0.1f);
+				Destroy(capsula1,0.1f);
+				Destroy(plano1,0.1f);
 				
-						} else {
-								Debug.Log ("exiten el objecto");
-						}
+						
 				}
 				if (GUI.Button (new Rect (250, 80, 100, 30), "Crear esfera")) {
-						if (esfera == 0) {
+					
 								pos1 += 3;
 								esfera1 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 								esfera1.transform.position = new Vector3 (-8, 0, pos1);
 								esfera1.transform.localScale = new Vector3 (2, 2, 2);
 								esfera1.transform.rotation = Quaternion.Euler (0, 0, 0);
 								esfera1.renderer.material.color = new Color (r, g, b);
-								esfera = 1;
-						} else {
-								Debug.Log ("exiten el objecto");
-						}
+								
+				
+				Destroy(cube1,0.1f);
+				Destroy(capsula1,0.1f);
+				Destroy(plano1,0.1f);
+
+
+						
 				}
 				if (GUI.Button (new Rect (250, 110, 100, 30), "Crear Capsula")) {
-						if (capsula == 0) {
+
 								pos2 += 3;
 								capsula1 = GameObject.CreatePrimitive (PrimitiveType.Capsule);
 								capsula1.transform.position = new Vector3 (-8, 0, pos2);
 								capsula1.transform.localScale = new Vector3 (2, 2, 2);
 								capsula1.transform.rotation = Quaternion.Euler (0, 0, 0);
 								capsula1.renderer.material.color = new Color (r, g, b);
-								capsula = 1;
-						} else {
-								Debug.Log ("exiten el objecto");
-						}
+								
+			Destroy(cube1,0.1f);
+			Destroy(esfera1,0.1f);
+			Destroy(plano1,0.1f);
+			
 				}
 				if (GUI.Button (new Rect (250, 140, 100, 30), "Crear Plano")) {
-						if (plano == 0) {
-								pos2 += 3;
-								plano1 = GameObject.CreatePrimitive (PrimitiveType.Plane);
-								plano1.transform.position = new Vector3 (-8, 0, pos2);
-								plano1.transform.localScale = new Vector3 (2, 2, 2);
-								plano1.transform.rotation = Quaternion.Euler (0, 0, 0);
-								plano1.renderer.material.color = new Color (r, g, b);
-								plano = 1;
-						} else {
-								Debug.Log ("exiten el objecto");
-						}
+					
+						pos2 += 3;
+						plano1 = GameObject.CreatePrimitive (PrimitiveType.Plane);
+						plano1.transform.position = new Vector3 (-8, 0, pos2);
+						plano1.transform.localScale = new Vector3 (2, 2, 2);
+						plano1.transform.rotation = Quaternion.Euler (0, 0, 0);
+						plano1.renderer.material.color = new Color (r, g, b);
+						
+						Destroy (cube1, 0.1f);
+						Destroy (esfera1, 0.1f);
+						Destroy (capsula1, 0.1f);
+				}	
 				}
-	}
 
 		void Update(){
 			if(cube1 != null)
